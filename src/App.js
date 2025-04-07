d4ef1459f8a642a3b0c34b1cbcacbf5c671640c5
PedroPablo <pedropablocarrascob@gmail.com>
2025-04-07 15:09:35 -0400

    Comenzando a agregar estilos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
 
+const itemStyle = {
+  padding: "1em",
+  borderBottom: "1px solid #ccc",
+  marginTop: "0.4em",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+const buttonStyle = {
+  background: "#ff4444",
+  color: "white",
+  border: "none",
+  borderRadius: "50%",
+  cursor: "pointer",
+  width: "24px",
+  height: "24px",
+};
+
+const appStyle = {
+  maxWidth: "600px",
+  margin: "0 auto",
+  padding: "2em",
+  fontFamily: "Arial, sans-serif",
+};
+
+class Item extends Component {
+  handleClick = () => {
+    this.props.onDelete(this.props.item.id);
+  };
+
+  render() {
+    return (
+      <div style={itemStyle}>
+        {this.props.item.text}
+        <button style={buttonStyle} onClick={this.handleClick}>
+          x
+        </button>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   state = {
-    list: []
+    list: [],
   };
 
   agregar = (e) => {
@@ -10,26 +53,34 @@ class App extends Component {
     const text = e.target[0].value;
     const id = Math.random().toString(16);
     const tarea = { text, id };
-    this.setState(state => ({
-      list: [...state.list, tarea]
+    this.setState((state) => ({
+      list: [...state.list, tarea],
     }));
-    e.target[0].value = ""; // Limpiar el input después de agregar
+    e.target[0].value = "";
+  };
+
+  eliminarTarea = (id) => {
+    this.setState((state) => ({
+      list: state.list.filter((item) => item.id !== id),
+    }));
   };
 
   render() {
     return (
-      <div className="App">
+      <div style={appStyle}>
         <h1>shouldComponentUpdate</h1>
         <form onSubmit={this.agregar}>
           <input type="text" placeholder="Ingresa tu Tarea" />
           <button type="submit">Añadir</button>
         </form>
-        
+
         {/* Lista de tareas */}
         {this.state.list.map((item) => (
-          <div key={item.id}>
-            {item.text}
-          </div>
+          <Item
+            key={item.id}
+            item={item}
+            onDelete={this.eliminarTarea}  // Pasamos la función de eliminación
+          />
         ))}
       </div>
     );
