242a6cf06780aa4a37d06d4ef4aa2652b2080a76
PedroPablo <pedropablocarrascob@gmail.com>
2025-04-07 12:42:39 -0400

    Mostrar datos en pantalla

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    list: []
+  };
+
+  agregar = (e) => {
+    e.preventDefault();
+    const text = e.target[0].value;
+    const id = Math.random().toString(16);
+    const tarea = { text, id };
+    this.setState(state => ({
+      list: [...state.list, tarea]
+    }));
+    e.target[0].value = ""; // Limpiar el input después de agregar
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>shouldComponentUpdate</h1>
+        <form onSubmit={this.agregar}>
+          <input type="text" placeholder="Ingresa tu Tarea" />
+          <button type="submit">Añadir</button>
+        </form>
+        
+        {/* Lista de tareas */}
+        {this.state.list.map((item) => (
+          <div key={item.id}>
+            {item.text}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
